2d38e8312fa7b2679eac0697fdcc9240afdc29b4
Nehemiah Sam <nlsam4028@gmail.com>
2022-06-14 15:30:10 -0500

    Azure Updates

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc:{
         clientId: '0oa5869i7kdYEwxh95d7',
         issuer: 'https://dev-19793492.okta.com/oauth2/default',
-        redirectUri: 'https://localhost:4200/login/callback',
+        redirectUri: window.location.origin + '/login/callback',
         scopes: ['openid', 'profile','email']
     }
 }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
-};
+  export const environment = {
+    production: true,
+    luv2shopApiUrl:"https://ecombackend.azurewebsites.net",
+    //luv2shopApiUrl:"https://localhost:8443/api",
+    stripePublishableKey: "pk_test_51L6bCaCGLhmINUsXGoXhgk6EWT5z6Rtj0199DBNqh3Sl5Y5jj4T2kQg9LovfcvvYmoAMJ0YdlmJO9981LDTTXZjJ00kwWFFqy3"
+  };
+
