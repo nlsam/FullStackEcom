8807de904011140ba3b94a64f6ad4768ec08c3af
Nehemiah Sam <nlsam4028@gmail.com>
2022-06-14 16:24:45 -0500

    More updates

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
   export const environment = {
     // new comment
     production: true,
-    luv2shopApiUrl:"https://ecombackend.azurewebsites.net",
+    luv2shopApiUrl:"https://ecombackend.azurewebsites.net/api",
     //luv2shopApiUrl:"https://localhost:8443/api",
     stripePublishableKey: "pk_test_51L6bCaCGLhmINUsXGoXhgk6EWT5z6Rtj0199DBNqh3Sl5Y5jj4T2kQg9LovfcvvYmoAMJ0YdlmJO9981LDTTXZjJ00kwWFFqy3"
   };
