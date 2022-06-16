2caa5d97131cc5b8546577b9dd8b4490e1088f7a
nlsam <78176096+nlsam@users.noreply.github.com>
2022-06-16 13:21:16 -0500

    Update my-app-config.ts

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc:{
-        clientId: '0oa5869i7kdYEwxh95d7',
-        issuer: 'https://dev-19793492.okta.com/oauth2/default',
+        clientId: '0oa4c8dn9p65fmC7O5d7',
+        issuer: 'https://dev-5165573.okta.com/oauth2/default',
         redirectUri: window.location.origin + '/login/callback',
         scopes: ['openid', 'profile','email']
     }
