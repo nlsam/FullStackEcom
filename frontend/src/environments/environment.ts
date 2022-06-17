f930a648a1eec6ee30b9063981f9b9ea250cca6e
nlsam <78176096+nlsam@users.noreply.github.com>
2022-06-17 13:06:47 -0500

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  luv2shopApiUrl:"https://localhost:8443/api",
+  luv2shopApiUrl:"https://ecombackend.azurewebsites.net/api",
   stripePublishableKey: "pk_test_51L6bCaCGLhmINUsXGoXhgk6EWT5z6Rtj0199DBNqh3Sl5Y5jj4T2kQg9LovfcvvYmoAMJ0YdlmJO9981LDTTXZjJ00kwWFFqy3"
 };
 
